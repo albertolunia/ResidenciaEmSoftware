e1e5b70426442a4233c589f0d81a08c7eb0677b7
albertolunia <albertolunia@gmail.com>
2023-08-30 11:43:09 -0300

    Att

--- InstrucaoPratica5/Exercicio5/ExercicioA.cpp
+++ InstrucaoPratica5/Exercicio5/ExercicioA.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main() {
-    unsigned int gene;
+    unsigned char gene1, gene2, gene3, gene4, gene5, gene6, gene7, gene8, gene, final;
+
+    gene1 = 1;
+    gene2 = 2;
+    gene3 = 4;
+    gene4 = 8;
+    gene5 = 16;
+    gene6 = 32;
+    gene7 = 64;
+    gene8 = 128;
+
+    cout << "Digite o gene da planta: ";
+    cin >> gene;
 
     
-
-
 }
