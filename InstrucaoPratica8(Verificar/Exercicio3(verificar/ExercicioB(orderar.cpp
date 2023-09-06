24d723f8f639b1bf6b0bd46077574cb05bcd0679
albertolunia <albertolunia@gmail.com>
2023-09-06 10:14:18 -0300

    Lista 7 corrigida

--- InstrucaoPratica8(Verificar/Exercicio3(verificar/ExercicioB(orderar.cpp
+++ InstrucaoPratica8(Verificar/Exercicio3(verificar/ExercicioB(orderar.cpp
@@ -1,10 +1,3 @@
-/*A seguinte expressão em C++ gera caracteres aleatórios no intervalo das
-letras minúsculas: ‘a’ + rand()%(‘z’ - ‘a’). Utilizando esta
-expressão implemente uma aplicação em que:
-a. Gere duas strings de forma aleatória com 10 caracteres;
-b. Transforme o primeiro caractere de cada string em maiúscula;
-c. Imprima as strings em ordem alfabética;*/
-
 #include <iostream>
 
 using namespace std;
